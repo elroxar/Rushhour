a95bb451af75cadcb0b86b6ca9fc74f0aa3c49e4
Theo Thiedig <theo@thiedig.net>
2018-10-30 23:37:57 +0100

    Changed format

--- src/Draw.java
+++ src/Draw.java
@@ -2,18 +2,22 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.Graphics;
-
+/**
+ * @author ben
+ * @version 30/10/18
+ */
 public class Draw extends Canvas
 {
    public Draw()
    {
-	 
   	 setSize(900,900);
   	 setBackground(Color.black);
    }
-
-
-public void paint(Graphics arg)
+	/**
+	 *
+	 * @param arg
+	 */
+	public void paint(Graphics arg)
    {
 	  //Vierecke 100,100 Platz zwischen feldern 42
 	  arg.setColor(Color.blue);
@@ -22,6 +26,6 @@ public void paint(Graphics arg)
 	  arg.drawLine(850,50,850,334);//oben nach unten rechts 1
 	  arg.drawLine(850,518,850,850);//oben nach unten rechts 2
 	  arg.drawLine(50,850,850,850);//links nach rechts unten 
-	  System.out.println("baaaaaggggg");
+	  //System.out.println("baaaaaggggg");
    }
-}
+}//Ende Klasse: Draw
