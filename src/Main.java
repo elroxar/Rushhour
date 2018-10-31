1ec12d870d398e5702ef06ff9401a419345c9679
Theo Thiedig <theo@thiedig.net>
2018-10-31 16:37:32 +0100

    Style Update

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,20 @@ import java.awt.event.*;
 import java.awt.*;
 public class Main extends JFrame 
 {
-   private Draw zgenerate;
-  public Main() 
-  {
+	/* Attribute */
+	private Draw zgenerate;
 	
-	setLocation(0,0);
-	Container cp = getContentPane();
-	cp.setLayout(new FlowLayout());
-	zgenerate = new Draw();
-	cp.add(zgenerate);
-	setUndecorated(true);
-	setSize((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight());
-	setAlwaysOnTop(true);
-	setVisible(true);
-  }
+	/* Methoden */
+    public Main()
+    {
+		setLocation(0,0);
+		Container cp = getContentPane();
+		cp.setLayout(new FlowLayout());
+		zgenerate = new Draw();
+		cp.add(zgenerate);
+		setUndecorated(true);
+		setSize((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+		setAlwaysOnTop(true);
+		setVisible(true);
+    }
 }
