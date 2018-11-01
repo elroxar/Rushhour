f631c387503a8343473a64c3104eea193835ee73
throvn <Faceformer1>
2018-11-01 18:04:03 +0100

    updatated version of rushhour

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,22 @@ import java.awt.event.*;
 import java.awt.*;
 public class Main extends JFrame 
 {
-	/* Attribute */
-	private Draw zgenerate;
+  private JLabel zZuege;
+  private Draw zgenerate;
+  public Main() 
+  {
 	
-	/* Methoden */
-    public Main()
-    {
-		setLocation(0,0);
-		Container cp = getContentPane();
-		cp.setLayout(new FlowLayout());
-		zgenerate = new Draw();
-		cp.add(zgenerate);
-		setUndecorated(true);
-		setSize((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight());
-		setAlwaysOnTop(true);
-		setVisible(true);
-    }
+	setLocation(250,10);
+	Container cp = getContentPane();
+	cp.setLayout(new FlowLayout());
+	zgenerate = new Draw();
+	cp.add(zgenerate);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setSize(1200,800);
+	setAlwaysOnTop(true);
+	setVisible(true);
+	setResizable(true);
+    cp.add(zZuege);
+	zZuege.setLocation(300,20);
+  }
 }
