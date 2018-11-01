4ad9f12ba0a3990f1fd57a30d78949ec0675e8ef
Theo Thiedig <theo@thiedig.net>
2018-11-01 19:14:41 +0100

    Wrote Tests

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        Spielbrett dasBrett = new Spielbrett();
-        dasBrett.fuelleSpielbrettKoord();
+        Main newGame = new Main();
     }
 
 }
