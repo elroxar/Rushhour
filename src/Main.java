a16effd63a61b245cac9f25f300210b9e90fa1ac
Theo Thiedig <29694509+elroxar@users.noreply.github.com>
2018-11-01 18:09:40 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,29 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-public class Main extends JFrame 
-{
-  private JLabel zZuege;
-  private Draw zgenerate;
-  public Main() 
-  {
+/**
+*
+*/
+public class Main extends JFrame {
+	/* Attribute */
+  	private JLabel zZuege;
+ 	 private Draw zgenerate;
 	
-	setLocation(250,10);
-	Container cp = getContentPane();
-	cp.setLayout(new FlowLayout());
-	zgenerate = new Draw();
-	cp.add(zgenerate);
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setSize(1200,800);
-	setAlwaysOnTop(true);
-	setVisible(true);
-	setResizable(true);
-    cp.add(zZuege);
-	zZuege.setLocation(300,20);
-  }
+	/* Methoden */
+ 	 public Main() 
+	  {
+		
+		setLocation(250,10);
+		Container cp = getContentPane();
+		cp.setLayout(new FlowLayout());
+		zgenerate = new Draw();
+		cp.add(zgenerate);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1200,800);
+		setAlwaysOnTop(true);
+		setVisible(true);
+		setResizable(true);
+		cp.add(zZuege);
+		zZuege.setLocation(300,20);
+  	}
 }
