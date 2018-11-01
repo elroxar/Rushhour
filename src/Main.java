78834fdd3842999eb0af6896245f43bd4366de60
weeebbeb <ben180701@gmail.com>
2018-11-02 00:10:58 +0100

    new push

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,71 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
+<<<<<<< Updated upstream
+
 /**
 *
 */
-public class Main extends JFrame {
+public class Main extends JFrame implements MouseListener
+{
 	/* Attribute */
   	private JLabel zZuege;
     private Draw zgenerate;
-	
-	/* Methoden */
- 	public Main() {
-		zZuege = new JLabel("Zug: ");
-		setLocation(250,10);
-		Container cp = getContentPane();
-		cp.setLayout(new FlowLayout());
-		zgenerate = new Draw();
-		cp.add(zgenerate);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(1200,800);
-		setAlwaysOnTop(true);
-		setVisible(true);
-		setResizable(true);
-		cp.add(zZuege);
-		zZuege.setLocation(300,20);
-  	}
-  	public void update(){
- 	 	//TODO: Implementieren
+=== private JMenuBar MenueLeiste;
+    private JMenu Levelauswahl;
+    private JMenuItem Level;
+public class Main extends JFrame {
+    /* Attribute */
+    private Draw zgenerate;
+>>>>>>>
+    Stashed changes
+
+    /* Methoden */
+    public Main() {
+        zZuege = new JLabel("Zug: ");
+        setLocation(250, 10);
+        Container cp = getContentPane();
+        cp.setLayout(new FlowLayout());
+        zgenerate = new Draw();
+        cp.add(zgenerate);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1200, 800);
+        setAlwaysOnTop(true);
+        setVisible(true);
+        setResizable(true);
+        cp.add(zZuege);
+        MenueLeiste = new JMenuBar();
+        Levelauswahl = new JMenu("LevelAuswahl");
+
+        for (int i = 1; i < 40; i++)
+        {
+            Levelauswahl.add(new Level("Level"+i));
+        }
+    }
+
+    public void update(){} //ist die mouseClicked Methode
+
+
+
+    public void mouseClicked(MouseEvent e)//wenn Maus geklickt wurde//Feldabfrage//e.getx/y
+    {
+      zgenerate.repaint();
+      if()
+    }
+
+    public void mousePressed(MouseEvent e)//wenn Maustaste runtergedrückt wurde
+    {
+    }
+
+    public void mouseReleased(MouseEvent e)//wenn Taste wieder losgelassen wurde
+    {
+    }
+
+    public void mouseEntered(MouseEvent e)//Maus in der Komponente
+    {
+    }
+
+    public void mouseExited(MouseEvent e)//Maus aus Komponente verschwunden
+    {
     }
 }
