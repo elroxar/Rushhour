dc877a72e8e963b81b57c5c63efff5ab3024252a
weeebbeb <ben180701@gmail.com>
2018-11-05 20:41:30 +0100

    debug

--- src/Draw.java
+++ src/Draw.java
@@ -2,34 +2,31 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Toolkit;
-/**
- * @author ben, louis
- * @version 05/11/2018
- */
+//autor Louis,Ben
 public class Draw extends Canvas {
-	/* Attribute */
+
 	final int breite = 95;
 
-	/* Methoden */
 	public Draw()
 	{
-
 		setSize(800,800);
 
+
+
 	}
-	/**
-	 * @param arg Graphics Object
-	 */
+
 	@Override
 	public void paint(Graphics arg) {
 		// Vierecke 95,95 Platz zwischen feldern 16///
+		setLocation((getParent().getWidth()/2)-(getWidth()/2),(getParent().getHeight()/2)-(getHeight()/2));//getParent() = uebergeordneter Container
+		arg.fillRect(0,0,getWidth(),getHeight());
 		arg.setColor(Color.black);
 		arg.drawRect(50, 50, 700, 700);
 
 		for (int x = 0; x < 6; x++)
 			for (int y = 0; y < 6; y++)
 				arg.drawRect(75 + (breite + 16) * x, 75 + (breite + 16) * y, breite, breite);
-		        arg.clearRect(749, 297, 3, 96);
+		arg.clearRect(749, 297, 3, 96);
 
 	}
 }
