cd31ea9615008e843d4dd4ba3f4fe49fcdb3d9d0
weeebbeb <ben180701@gmail.com>
2018-11-05 21:53:22 +0100

    autogenerate/draw vehicles

--- src/Draw.java
+++ src/Draw.java
@@ -4,15 +4,13 @@ import java.awt.Graphics;
 import java.awt.Toolkit;
 //autor Louis,Ben
 public class Draw extends Canvas {
-
+	private Fahrzeug [] zFahrezuge;
 	final int breite = 95;
 
-	public Draw()
+	public Draw(Farzeueg [] pFahrzeuge)
 	{
 		setSize(800,800);
-
-
-
+		zFahrezuge = pFahrzeuge;
 	}
 
 	@Override
@@ -27,6 +25,40 @@ public class Draw extends Canvas {
 			for (int y = 0; y < 6; y++)
 				arg.drawRect(75 + (breite + 16) * x, 75 + (breite + 16) * y, breite, breite);
 		arg.clearRect(749, 297, 3, 96);
-
+		drawFahrzeuge(Graphics arg1);
 	}
-}
+	//generiere Fahrzeuge ABER noch ohne verschiebeMethode
+	public void drawFahrzeuge(Graphics arg1) {
+		for (int i = 0; i < zFarzeuge.length; i++) {
+			if (zFahrzeuge[i].istVertikal())//vertikal?
+			{
+				if (zFahrzeuge[i] instanceof PKW)//PKW?
+				{
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite, breite + 16);
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2() - 16, breite, breite);
+
+				} else {
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite, breite + 16);
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2(), breite, breite);
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX3(), 75 + (breite + 16) * zFahrzeuge[i].gibY3() - 16, breite, breite);
+				}
+
+
+
+
+			} else {
+				if (zFahrzeuge[i] instanceof PKW)//PKW?
+				{
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite+16, breite);
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2() - 16, breite, breite);
+
+				} else {
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite+16, breite);
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2(), breite, breite);
+					arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX3(), 75 + (breite + 16) * zFahrzeuge[i].gibY3() - 16, breite, breite);
+				}
+
+
+			}
+		}
+	}
