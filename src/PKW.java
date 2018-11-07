bcb0688ad05be3ac54d28d064228d397023027a6
Theo Thiedig <theo@thiedig.net>
2018-11-07 08:40:56 +0100

    Updated Comments

--- src/PKW.java
+++ src/PKW.java
@@ -5,21 +5,21 @@ import java.awt.Graphics;
  * @version 30/10/18
  */
 public class PKW extends Fahrzeug {
-    /* Attribute*/
-    private boolean zIstRotesAuto;
-    
-    /* Methoden *///
-    PKW(int pX1, int pY1, int pX2, int pY2, boolean pIstRotesAuto) {
-        super(pX1, pY1, pX2, pY2);
-        zIstRotesAuto = pIstRotesAuto;
-        if(this.istRotesAuto())
-        	super.zFarbe = Color.RED;
-    }
-    /**
-     * @return liefert, ob das Auto das 'rote' Auto ist
-     */
-    public boolean istRotesAuto(){
-        return this.zIstRotesAuto;
-    }
-
+	/* Attribute*/
+	private boolean zIstRotesAuto;
+	
+	/* Methoden *///
+	PKW(int pX1, int pY1, int pX2, int pY2, boolean pIstRotesAuto) {
+		super(pX1, pY1, pX2, pY2);
+		zIstRotesAuto = pIstRotesAuto;
+		if(this.istRotesAuto())
+			super.zFarbe = Color.RED;
+	}
+	/**
+	 * @return liefert, ob das Auto das 'rote' Auto ist
+	 */
+	public boolean istRotesAuto(){
+		return this.zIstRotesAuto;
+	}
+	
 }//Ende Klasse: PKW
