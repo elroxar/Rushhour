f84a0940662987f3e6d70d3b7598b3db809779ae
throvn <Faceformer1>
2018-11-07 11:47:00 +0100

    fixed Level choosing

--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,18 @@ public class Main extends JFrame implements MouseListener {
 	private JMenu zMNgradMittel;
 	private JMenu zMNgradSchwer;
 	private JMenu zMNgradExtrem;
-	private Fahrzeug [] zFahrzeuge;
+	private Fahrzeug[] zFahrzeuge;
+	private Spielbrett zSpielbrett = new Spielbrett();
+
+	private JMenuItem[] zLevel = new JMenuItem[40];
+	private int lI;
 
     /* Methoden */
     public Main() {
 
 		//Standardeinstellungen
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(),(int)Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+		setSize((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight());
 		setAlwaysOnTop(true);
 		setVisible(true);
 		setResizable(true);
@@ -34,22 +38,22 @@ public class Main extends JFrame implements MouseListener {
 		cp.setLayout(new FlowLayout());
 		cp.setBackground(Color.BLACK);
 
-        
-        //initialisiere zGenerate
-        zGenerate = new Draw();
-        cp.add(zGenerate);
+
+		//initialisiere zGenerate
+		zGenerate = new Draw();
+		cp.add(zGenerate);
 
 		//Print Spielzuege
 		zLBSpielzuege = new JLabel("Zug: ");
 		cp.add(zLBSpielzuege);
 
-        //Erstelle Menueleiste
-        zMBMenueleiste = new JMenuBar();
+		//Erstelle Menueleiste
+		zMBMenueleiste = new JMenuBar();
 		setJMenuBar(zMBMenueleiste);
 
 		//Initialisieren der Menueleisten Gegenstaende
 		zMNLevelauswahl = new JMenu("LevelAuswahl");
-        zMNgradLeicht = new JMenu("Leichte Level");
+		zMNgradLeicht = new JMenu("Leichte Level");
 		zMNgradMittel = new JMenu("Normale Level");
 		zMNgradSchwer = new JMenu("Schwere Level");
 		zMNgradExtrem = new JMenu("Extreme Level");
@@ -62,17 +66,29 @@ public class Main extends JFrame implements MouseListener {
 		zMNLevelauswahl.add(zMNgradExtrem);
 
 		//Level nach Schwierigkeitsgrd sortieren
-        for (int i = 1; i <= 40; i++)
+		JMenuItem lLevel = new JMenuItem();
+		for (int i = 0; i < 40; i++) {
+
+			lLevel = new JMenuItem(("Level " + i));
+			lI = i;
+			lLevel.addActionListener((ActionEvent ev) -> {
+				JMenuItem obj = (JMenuItem)ev.getSource();
+				String id = obj.getText().split(" ")[1];
+				zSpielbrett.ladeLevel(Integer.parseInt(id));
+			});
+			zLevel[i] = lLevel;
 			if (i <= 10) {
-				zMNgradLeicht.add(new JMenuItem("Level " + i)); //Hinzufuegen des levels
+				zMNgradLeicht.add(zLevel[i]); //Hinzufuegen des levels
 			} else if (i <= 20) {
-				zMNgradMittel.add(new JMenuItem("Level " + i)); //Hinzufuegen des levels
+				zMNgradMittel.add(zLevel[i]); //Hinzufuegen des levels
 			} else if (i <= 30) {
-				zMNgradSchwer.add(new JMenuItem("Level " + i)); //Hinzufuegen des levels
+				zMNgradSchwer.add(zLevel[i]); //Hinzufuegen des levels
 			} else if (i <= 40) {
-				zMNgradExtrem.add(new JMenuItem("Level " + i)); //Hinzufuegen des levels
+				zMNgradExtrem.add(zLevel[i]); //Hinzufuegen des levels
 			}
-    }
+
+		}
+	}
     /**
      *
      */
