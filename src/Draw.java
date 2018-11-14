3e01c919545fa7577de51ca1b17da0c9f18f93c9
throvn <Faceformer1>
2018-11-14 19:06:49 +0100

    Vehicles painted

--- src/Draw.java
+++ src/Draw.java
@@ -11,7 +11,6 @@ public class Draw extends Canvas {
 	private Graphics arg1;
 	public Draw() {
 		setSize(800, 800);
-
 		//zFahrzeuge = zSpielbrett.ladeLevel();
 		//drawFahrzeuge(zFahrzeuge);
 	}
@@ -24,47 +23,90 @@ public class Draw extends Canvas {
 	public void paint(Graphics arg) {
 		arg1 = arg;
 		// Vierecke 95,95 Platz zwischen feldern 16///
-		setBackground(Color.RED);
-		setLocation((getParent().getWidth() / 2) - (getWidth() / 2), (getParent().getHeight() / 2) - (getHeight() / 2));//getParent() = uebergeordneter Container
-		arg.fillRect(0, 0, getWidth(), getHeight());
-		arg.setColor(Color.WHITE);
-		arg.drawRect(50, 50, 700, 700);
+		//setLocation((getParent().getWidth() / 2) - (getWidth() / 2), (getParent().getHeight() / 2) - (getHeight() / 2));//getParent() = uebergeordneter Container
+
+
+
 		
 		for(int x = 0; x < 6; x++)
 			for(int y = 0; y < 6; y++)
 				arg.drawRect(75 + (breite + 16) * x, 75 + (breite + 16) * y, breite, breite);
 		arg.clearRect(749, 297, 3, 96);
-		//drawFahrzeuge(Graphics arg1);
+		//drawFahrzeuge(pFahrzeuge);
+
+		for(int i = 0; i < zFahrzeuge.length; i++) {
+			if(zFahrzeuge[i].istVertikal())//vertikal?
+			{
+				//zeichne( i, breite, breite + 16);
+				if(zFahrzeuge[i] instanceof PKW)
+				//PKW?
+				{
+					arg1.setColor(zFahrzeuge[i].gibFarbe());
+					if (zFahrzeuge[i].gibX1() < zFahrzeuge[i].gibX2()){
+						arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2(), breite, 2*breite+16);
+						arg1.setColor(Color.BLACK);
+						arg1.drawRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2(), breite, 2*breite+16);
+					}
+					else {
+						arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite, 2*breite+16);
+						arg1.setColor(Color.BLACK);
+						arg1.drawRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite, 2*breite+16);
+					}
+				}
+				else if(zFahrzeuge[i] instanceof LKW){
+					arg1.setColor(zFahrzeuge[i].gibFarbe());
+					if (zFahrzeuge[i].gibX1() < zFahrzeuge[i].gibX2()){
+						arg1.fillRect(75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibX3(), 75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibY3(), breite,3*breite+32);
+						arg1.setColor(Color.BLACK);
+						arg1.drawRect(75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibX3(), 75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibY3(), breite,3*breite+32);
+					}
+					else {
+						arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite, 3*breite+32);
+						arg1.setColor(Color.BLACK);
+						arg1.drawRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite, 3*breite+32);
+					}
+				}
+			}
+			else {
+				//zeichne( i, breite + 16, breite);
+				if(zFahrzeuge[i] instanceof PKW)
+				//PKW?
+				{
+					arg1.setColor(zFahrzeuge[i].gibFarbe());
+					if (zFahrzeuge[i].gibX1() < zFahrzeuge[i].gibX2()){
+						arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), 2*breite+16, breite);
+						arg1.setColor(Color.BLACK);
+						arg1.drawRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), 2*breite+16, breite);
+					}
+					else {
+						arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2(), 2*breite+16, breite);
+						arg1.setColor(Color.BLACK);
+						arg1.drawRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2(), 2*breite+16, breite);
+					}
+				}
+				else if(zFahrzeuge[i] instanceof LKW){
+					arg1.setColor(zFahrzeuge[i].gibFarbe());
+					if (zFahrzeuge[i].gibX1() < ((LKW)zFahrzeuge[i]).gibX3()){
+						arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), 3*breite+32, breite);
+						arg1.setColor(Color.BLACK);
+						arg1.drawRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), 3*breite+32, breite);
+					}
+					else {
+						arg1.fillRect(75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibX3(), 75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibY3(), 3*breite+32, breite);
+						arg1.setColor(Color.BLACK);
+						arg1.drawRect(75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibX3(), 75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibY3(), 3*breite+32, breite);
+					}
+
+				}
+			}
+		}
 	}
 
 	//generiere Fahrzeuge ABER noch ohne verschiebeMethode
 	public void drawFahrzeuge(Fahrzeug[] pFahrzeuge) {
-		zFahrzeuge = pFahrzeuge;;
-		for(int i = 0; i < pFahrzeuge.length; i++) {
-			if(pFahrzeuge[i].istVertikal())//vertikal?
-			{
-				zeichne( i, breite, breite + 16);
-			}
-			else {
-				zeichne( i, breite + 16, breite);
-			}
-		}
+		zFahrzeuge = pFahrzeuge;
 	}
 	
 	private void zeichne( int i, int breite, int i2) {
-
-		if(zFahrzeuge[i] instanceof PKW)
-			//PKW?
-		{
-			arg1.setColor(zFahrzeuge[i].gibFarbe());
-			arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite, i2);
-			arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2() - 16, breite, breite);
-		}
-		else if(zFahrzeuge[i] instanceof LKW){
-			arg1.setColor(zFahrzeuge[i].gibFarbe());
-			arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX1(), 75 + (breite + 16) * zFahrzeuge[i].gibY1(), breite, i2);
-			arg1.fillRect(75 + (breite + 16) * zFahrzeuge[i].gibX2(), 75 + (breite + 16) * zFahrzeuge[i].gibY2(), breite, breite);
-			arg1.fillRect(75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibX3(), 75 + (breite + 16) * ((LKW)zFahrzeuge[i]).gibY3() - 16, breite, breite);
-		}
 	}
 }
