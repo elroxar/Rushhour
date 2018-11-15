f508bd1b7028f51670ec3e4157fa0e5fe63f6504
weeebbeb <ben180701@gmail.com>
2018-11-15 16:44:52 +0100

    CustomButton Push

--- src/StartFenster.java
+++ src/StartFenster.java
@@ -2,76 +2,45 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 import java.awt.Graphics;
+
 /**
  * @author ben
  * @version 05/11/2018
  */
-public class StartFenster extends JFrame implements MouseListener
+public class StartFenster extends JFrame
 {
 
     private JLabel zTitel;
-    private Button zStart,zEinstellungen;
+    private Startbutton zStart;
+    private Settingsbutton zEinstellungen;
+    private Icon zIcon;
+
     public StartFenster()
     {
-
+        super("Rushhour-The Game");
+        setSize(1200,800);
+        setLocation((int)(Toolkit.getDefaultToolkit().getScreenSize().getWidth()/2)-600,(int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2)-400);
         Container cp = getContentPane();
         cp.setLayout(null);
-        setLocation(100,100);
-        zTitel = new JLabel("Welcome to Rushhour");
-        zTitel.setSize(100,400);
-        zTitel.setLocation(550,200);
-        cp.add(zTitel);
-        setSize(1200,800);
-        zStart = new Button();
+
+        zStart = new Startbutton(Toolkit.getDefaultToolkit().getImage("play_button.jpg"),0,0);
         zStart.setSize(70,70);
-        //zStart.paint(Toolkit.getDefaultToolkit().getImage("data/rot.png"),450,400);
-
+        zStart.setLocation(450,400);
         cp.add(zStart);
-        zEinstellungen = new Button();
-        //zEinstellungen.paint(Toolkit.getDefaultToolkit().getImage("rot"),650,400);
 
+        zEinstellungen = new Settingsbutton(Toolkit.getDefaultToolkit().getImage("settings_button.jpg"),0,0);
+        zEinstellungen.setSize(70,70);
+        zEinstellungen.setLocation(650,400);
         cp.add(zEinstellungen);
+
+        zIcon = new Icon(Toolkit.getDefaultToolkit().getImage("Icon.jpg"),0,0);
+        zIcon.setSize(800,200);
+        zIcon.setLocation(200,100);
+        cp.add(zIcon);
+
+
         setVisible(true);
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public void actionPerformed(ActionEvent arg)
-    {
-        if(arg.getSource().equals(zStart))new Main();
-        //else if(arg.getSource().equals(zEinstellungen))new Einstellungen();
-
-    }
-    public void mouseClicked(MouseEvent e)//wenn Maus geklickt wurde//Feldabfrage//e.getx/y
-    {
-        //zGenerate.repaint();
-        //if(e.getx())
-    }
-    /**
-     *
-     * @param e MouseEvent Object
-     */
-    public void mousePressed(MouseEvent e)//wenn Maustaste runtergedrückt wurde
-    {
-    }
-    /**
-     *
-     * @param e MouseEvent Object
-     */
-    public void mouseReleased(MouseEvent e)//wenn Taste wieder losgelassen wurde
-    {
-    }
-    /**
-     *
-     * @param e MouseEvent Object
-     */
-    public void mouseEntered(MouseEvent e)//Maus in der Komponente
-    {
-    }
-    /**
-     *
-     * @param e MouseEvent Object
-     */
-    public void mouseExited(MouseEvent e)//Maus aus Komponente verschwunden
-    {
     }
 }
